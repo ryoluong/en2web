6bc53632b57393deb539a9825872ca5995f8202f
Ryo Kobayashi <cuba6vst@gmail.com>
2019-02-01 03:06:59 +0900

    testtt!

--- resources/views/layouts/web/head.blade.php
+++ resources/views/layouts/web/head.blade.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link type="text/css" rel="stylesheet" href="/css/en2webstyle.css?version=20190201">
+<link type="text/css" rel="stylesheet" href="/css/en2webstyle.css">
 <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
 
 <title>En2::Web</title>
@@ -57,7 +57,6 @@
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-
   gtag('config', 'UA-131467484-1');
 </script>
 @endif
