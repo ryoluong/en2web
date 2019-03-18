adb11f9d6a8615eabcb82b0872b217b4fe2d1fe9
Ryo Kobayashi <cuba6vst@gmail.com>
2019-03-18 23:24:00 +0900

    ht

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use App\Facades\Slack;
+use Illuminate\Support\Facades\Log;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,57 +25,57 @@ Route::post('/webhook/line', function(Request $request) {
     $json = json_decode($json_string, true);
     Log::info(var_dump($json_object));
     Log::info('LINE API works!');
-    $replyToken = $json->events[0]->replyToken;
-    // JSONデータから送られてきたメッセージを取得
-    $message = $json->events[0]->message->text;
+    // $replyToken = $json->events[0]->replyToken;
+    // // JSONデータから送られてきたメッセージを取得
+    // $message = $json->events[0]->message->text;
 
-    // HTTPヘッダを設定
-    $channelToken = 'sv/RKt1C3qskQg0Uh5Xdll9aXWvy42rty+y9gdYtQjzQ5AOMMKOgPPU6yTAuxkoRsTXsWVSmv648F5wXHJzpvWPCkUSnfdjuxj91YZIr7Np4rGPlgFFbsAFeyuL6I8nUFSYZCQvvEZkfHngYPfAtUgdB04t89/1O/w1cDnyilFU=';
-    $headers = [
-        'Authorization: Bearer ' . $channelToken,
-        'Content-Type: application/json; charset=utf-8',
-    ];
+    // // HTTPヘッダを設定
+    // $channelToken = 'sv/RKt1C3qskQg0Uh5Xdll9aXWvy42rty+y9gdYtQjzQ5AOMMKOgPPU6yTAuxkoRsTXsWVSmv648F5wXHJzpvWPCkUSnfdjuxj91YZIr7Np4rGPlgFFbsAFeyuL6I8nUFSYZCQvvEZkfHngYPfAtUgdB04t89/1O/w1cDnyilFU=';
+    // $headers = [
+    //     'Authorization: Bearer ' . $channelToken,
+    //     'Content-Type: application/json; charset=utf-8',
+    // ];
 
-    // POSTデータを設定してJSONにエンコード
-    $post = [
-        'replyToken' => $replyToken,
-        'messages' => [
-            [
-                'type' => 'text',
-                'text' => '「' . $message . '」',
-            ],
-        ],
-    ];
-    $post = json_encode($post);
+    // // POSTデータを設定してJSONにエンコード
+    // $post = [
+    //     'replyToken' => $replyToken,
+    //     'messages' => [
+    //         [
+    //             'type' => 'text',
+    //             'text' => '「' . $message . '」',
+    //         ],
+    //     ],
+    // ];
+    // $post = json_encode($post);
 
-    // HTTPリクエストを設定
-    $ch = curl_init('https://api.line.me/v2/bot/message/reply');
-    $options = [
-        CURLOPT_CUSTOMREQUEST => 'POST',
-        CURLOPT_HTTPHEADER => $headers,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_BINARYTRANSFER => true,
-        CURLOPT_HEADER => true,
-        CURLOPT_POSTFIELDS => $post,
-    ];
+    // // HTTPリクエストを設定
+    // $ch = curl_init('https://api.line.me/v2/bot/message/reply');
+    // $options = [
+    //     CURLOPT_CUSTOMREQUEST => 'POST',
+    //     CURLOPT_HTTPHEADER => $headers,
+    //     CURLOPT_RETURNTRANSFER => true,
+    //     CURLOPT_BINARYTRANSFER => true,
+    //     CURLOPT_HEADER => true,
+    //     CURLOPT_POSTFIELDS => $post,
+    // ];
 
-    // 実行
-    curl_setopt_array($ch, $options);
+    // // 実行
+    // curl_setopt_array($ch, $options);
 
-    // エラーチェック
-    $result = curl_exec($ch);
-    $errno = curl_errno($ch);
-    if ($errno) {
-        return;
-    }
+    // // エラーチェック
+    // $result = curl_exec($ch);
+    // $errno = curl_errno($ch);
+    // if ($errno) {
+    //     return;
+    // }
 
-    // HTTPステータスを取得
-    $info = curl_getinfo($ch);
-    $httpStatus = $info['http_code'];
+    // // HTTPステータスを取得
+    // $info = curl_getinfo($ch);
+    // $httpStatus = $info['http_code'];
 
-    $responseHeaderSize = $info['header_size'];
-    $body = substr($result, $responseHeaderSize);
+    // $responseHeaderSize = $info['header_size'];
+    // $body = substr($result, $responseHeaderSize);
 
-    // 200 だったら OK
-    echo $httpStatus . ' ' . $body;
+    // // 200 だったら OK
+    // echo $httpStatus . ' ' . $body;
 });
