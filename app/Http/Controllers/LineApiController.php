0ea0b2aed0bd8b9c0e6b983fccac7ab63ad9c205
Ryo Kobayashi <cuba6vst@gmail.com>
2019-03-22 23:05:45 +0900

    g

--- app/Http/Controllers/LineApiController.php
+++ app/Http/Controllers/LineApiController.php
@@ -3,15 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Facades\Slack;
 
 class LineApiController extends Controller
 {
-    protected $bot;
+    protected $url = 'https://api.line.me/v2/bot/message/push';
+    protected $channelToken = 'sv/RKt1C3qskQg0Uh5Xdll9aXWvy42rty+y9gdYtQjzQ5AOMMKOgPPU6yTAuxkoRsTXsWVSmv648F5wXHJzpvWPCkUSnfdjuxj91YZIr7Np4rGPlgFFbsAFeyuL6I8nUFSYZCQvvEZkfHngYPfAtUgdB04t89/1O/w1cDnyilFU=';
+
 
     public function __construct()
     {
-        $httpClient = new \LINE\LineBot\HTTPClient\CurlHttpClient('sv/RKt1C3qskQg0Uh5Xdll9aXWvy42rty+y9gdYtQjzQ5AOMMKOgPPU6yTAuxkoRsTXsWVSmv648F5wXHJzpvWPCkUSnfdjuxj91YZIr7Np4rGPlgFFbsAFeyuL6I8nUFSYZCQvvEZkfHngYPfAtUgdB04t89/1O/w1cDnyilFU=');
-        $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '7a7c6907e671492c584c734f071a3005']);
+        
     }
 
     public function response()
@@ -21,6 +23,8 @@ class LineApiController extends Controller
         $json_string = request()->getContent();
         $json_object = json_decode($json_string, true);
         $event = $json_object['events'][0];
-        $response = $bot->replyText($event->replyToken, 'hello!');
+        if($event->type == 'join') {
+            Slack::notice('Bot joined group! groupID: '.$event->source->groupId);
+        }
     }
 }
