8eb62d3c5fc69835f601273b383efb6c23cc885e
Ryo Kobayashi <cuba6vst@gmail.com>
2019-03-23 00:26:37 +0900

    aa

--- app/Http/Controllers/LineApiController.php
+++ app/Http/Controllers/LineApiController.php
@@ -35,41 +35,41 @@ class LineApiController extends Controller
                     "type" => "text",
                     "text" => "hihi",
                 ],
-                [
-                    "type" => "template",
-                    "altText" => "This is a buttons template",
-                    "template" => [
-                        "type" => "buttons",
-                        "thumbnailImageUrl" => "https =>//example.com/bot/images/image.jpg",
-                        "imageAspectRatio" => "rectangle",
-                        "imageSize" => "cover",
-                        "imageBackgroundColor" => "#FFFFFF",
-                        "title" => "Menu",
-                        "text" => "Please select",
-                        "defaultAction" => [
-                            "type" => "uri",
-                            "label" => "View detail",
-                            "uri" => "http =>//example.com/page/123"
-                        ],
-                        "actions" => [
-                            [
-                                "type" => "postback",
-                                "label" => "Buy",
-                                "data" => "action=buy&itemid=123"
-                            ],
-                            [
-                                "type" => "postback",
-                                "label" => "Add to cart",
-                                "data" => "action=add&itemid=123"
-                            ],
-                            [
-                                "type" => "uri",
-                                "label" => "View detail",
-                                "uri" => "http://example.com/page/123"
-                            ]
-                        ]
-                    ]
-                ],
+                // [
+                //     "type" => "template",
+                //     "altText" => "This is a buttons template",
+                //     "template" => [
+                //         "type" => "buttons",
+                //         "thumbnailImageUrl" => "https =>//example.com/bot/images/image.jpg",
+                //         "imageAspectRatio" => "rectangle",
+                //         "imageSize" => "cover",
+                //         "imageBackgroundColor" => "#FFFFFF",
+                //         "title" => "Menu",
+                //         "text" => "Please select",
+                //         "defaultAction" => [
+                //             "type" => "uri",
+                //             "label" => "View detail",
+                //             "uri" => "http =>//example.com/page/123"
+                //         ],
+                //         "actions" => [
+                //             [
+                //                 "type" => "postback",
+                //                 "label" => "Buy",
+                //                 "data" => "action=buy&itemid=123"
+                //             ],
+                //             [
+                //                 "type" => "postback",
+                //                 "label" => "Add to cart",
+                //                 "data" => "action=add&itemid=123"
+                //             ],
+                //             [
+                //                 "type" => "uri",
+                //                 "label" => "View detail",
+                //                 "uri" => "http://example.com/page/123"
+                //             ]
+                //         ]
+                //     ]
+                // ],
             ]);
             $options = array (
                 'http' => array (
