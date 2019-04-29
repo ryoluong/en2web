ec3e8cdc41bc77c519e4d0e59c775d15b10ab24e
User En2web-test <en2web-test@www4019.sakura.ne.jp>
2019-04-29 16:33:20 +0900

    alter .gitignore

---
+++ cpassets.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp -r /home/en2web-test/en2web/public/css/ /home/en2web-test/www/en2web/css/
+cp -r /home/en2web-test/en2web/public/js/ /home/en2web-test/www/en2web/js/
+cp -r /home/en2web-test/en2web/public/img/ /home/en2web-test/www/en2web/img/ 
