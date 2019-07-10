01715b002243c1fe89fe857ece35f65212432f71
Ryo Kobayashi <cuba6vst@gmail.com>
2019-07-11 00:33:27 +0900

    fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -123,14 +123,14 @@ class UserController extends Controller
         if ($action == 'update') {
             $user = auth()->user();
             $filename = 'avater_'.$user->id.'_'.uniqid().'.'.pathinfo($path, PATHINFO_EXTENSION);
-            Storage::disk('public')->move('/public' . $path, '/storage/img/user/'.$filename);
+            Storage::disk('storage')->move($path, '/img/user/'.$filename);
             if ($user->avater_path !== null) {
                 unlink(public_path('storage').$user->avater_path);
             }
             $user->update(['avater_path' => '/storage/img/user/'.$filename]);
             return redirect('/mypage');
         } else {
-            Storage::disk('public')->delete($path);
+            Storage::disk('storage')->delete($path);
             return redirect('/mypage/upload_avater');
         }
     }
@@ -169,14 +169,14 @@ class UserController extends Controller
         if ($action === 'update') {
             $user = auth()->user();
             $filename = 'coverimg_'.$user->id.'_'.uniqid().'.'.pathinfo($path, PATHINFO_EXTENSION);
-            Storage::disk('public')->move('/public' . $path, '/storage/img/user/'.$filename);
+            Storage::disk('storage')->move($path, '/img/user/'.$filename);
             if ($user->coverimg_path !== null) {
                 unlink(public_path('storage').$user->coverimg_path);
             }
             $user->update(['coverimg_path' => '/storage/img/user/'.$filename]);
             return redirect('/mypage');
         } else {
-            Storage::disk('public')->delete($path);
+            Storage::disk('storage')->delete($path);
             return redirect('/mypage/upload_coverimg');
         }
     }
